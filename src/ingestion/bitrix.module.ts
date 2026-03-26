b3d0464d7508e58c7e441fba15fbbab81520cd7e
Aleksandr <atman.oum@gmail.com>
2026-03-26 15:20:06 +0500

    fix: add missing imports to IngestionModule (TenantConfigModule, AggregationModule, LoggerModule)

--- src/ingestion/bitrix.module.ts
+++ src/ingestion/bitrix.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common'
+import { AggregationModule } from '../aggregation/aggregation.module'
+import { LoggerModule } from '../logger/logger.module'
+import { TenantConfigModule } from '../config/tenant-config.module'
 import { BitrixController } from './bitrix.controller'
 import { BitrixService } from './bitrix.service'
 
 @Module({
+  imports: [TenantConfigModule, AggregationModule, LoggerModule],
   controllers: [BitrixController],
   providers: [BitrixService],
 })
